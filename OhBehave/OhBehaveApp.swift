53386dc691a148e37d362fe66bcdb3627518616f
Ben Gottlieb <ben@standalone.com>
2021-12-22 08:24:44 -0500

    Initial Commit

---
+++ OhBehave/OhBehaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhBehaveApp.swift
+//  OhBehave
+//
+//  Created by Ben Gottlieb on 12/22/21.
+//
+
+import SwiftUI
+
+@main
+struct OhBehaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
